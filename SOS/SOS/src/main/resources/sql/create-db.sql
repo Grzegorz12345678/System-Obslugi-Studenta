362e059a8a46172cd7a0d1525dc9736b32cdfdc6
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-06-02 11:32:12 +0200

    Tworzenie skryptu tworzącego strukturę bazy danych.

--- SOS/SOS/src/main/resources/sql/create-db.sql
+++ SOS/SOS/src/main/resources/sql/create-db.sql
@@ -1,1 +1,14 @@
-..
+CREATE TABLE Student (
+    idStudenta int PRIMARY KEY,
+    numerIndeksu int UNIQUE NOT NULL,
+    imie varchar(255) NOT NULL,
+    nazwisko varchar(255) NOT NULL,
+    email varchar(255) UNIQUE NOT NULL,
+    telefon varchar(20),
+    ulica varchar(255),
+    miejscowosc varchar(255),
+    kodPocztowy varchar(10)
+    login varchar(255) UNIQUE NOT NULL,
+    haslo varchar(255) NOT NULL,
+    blokada boolean NOT NULL
+);
