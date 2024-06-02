48f89ad7e1cea5f64cd0a02e46c6775c5ba0c681
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-06-02 11:51:32 +0200

    Tworzenie skryptu tworzącego strukturę bazy danych.

--- SOS/SOS/src/main/resources/sql/create-db.sql
+++ SOS/SOS/src/main/resources/sql/create-db.sql
@@ -1,14 +1,51 @@
 CREATE TABLE Student (
-    idStudenta int PRIMARY KEY,
-    numerIndeksu int UNIQUE NOT NULL,
-    imie varchar(255) NOT NULL,
-    nazwisko varchar(255) NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
-    telefon varchar(20),
-    ulica varchar(255),
-    miejscowosc varchar(255),
-    kodPocztowy varchar(10)
-    login varchar(255) UNIQUE NOT NULL,
-    haslo varchar(255) NOT NULL,
-    blokada boolean NOT NULL
+    idStudenta INT PRIMARY KEY,
+    numerIndeksu INT UNIQUE NOT NULL,
+    imie VARCHAR(255) NOT NULL,
+    nazwisko VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    login VARCHAR(255) UNIQUE NOT NULL,
+    haslo VARCHAR(255) NOT NULL,
+    zablokowany BOOLEAN NOT NULL,
+    telefon VARCHAR(20),
+    ulica VARCHAR(255),
+    miejscowosc VARCHAR(255),
+    kodPocztowy VARCHAR(10)
+);
+
+CREATE TABLE Przedmiot (
+    idPrzedmiot INT PRIMARY KEY,
+    nazwaPrzedmiotu VARCHAR(255) NOT NULL,
+    opisPrzedmiotu VARCHAR(255)
+);
+
+CREATE TABLE Plan_Zajec (
+    idPlanu INT PRIMARY KEY,
+    dataZajec DATE NOT NULL,
+    godzinaRozpoczecia TIME NOT NULL,
+    godzinaZakonczenia TIME NOT NULL,
+    idPrzedmiot INT,
+    FOREIGN KEY (idPrzedmiot) REFERENCES Przedmiot(idPrzedmiot)
+);
+
+CREATE TABLE Wykladowca (
+    idWykladowcy INT PRIMARY KEY,
+    imie VARCHAR(255) NOT NULL,
+    nazwisko VARCHAR(255) NOT NULL,
+    tytulNaukowy VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    haslo VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Ocena (
+    idOceny INT PRIMARY KEY,
+    wartosc FLOAT NOT NULL,
+    dataWystawienia TIMESTAMP NOT NULL,
+    komentarz VARCHAR(255),
+    idStudent INT,
+    idPrzedmiot INT,
+    idWykladowcy INT,
+    FOREIGN KEY (idStudent) REFERENCES Student(idStudenta),
+    FOREIGN KEY (idPrzedmiot) REFERENCES Przedmiot(idPrzedmiot),
+    FOREIGN KEY (idWykladowcy) REFERENCES Wykladowca(idWykladowcy)
 );
