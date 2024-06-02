6a1b0f26c1c29277b347390fbec480ffb29ac6af
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-06-02 13:55:29 +0200

    Tworzenie skryptu tworzącego strukturę bazy danych.

---
+++ SOS/SOS/src/main/resources/sql/insert-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Przedmiot (idPrzedmiot, nazwaPrzedmiotu, opisPrzedmiotu) VALUES
+(1, 'Matematyka', 'Podstawy analizy matematycznej'),
+(2, 'Informatyka', 'Wprowadzenie do programowania'),
+(3, 'Fizyka', 'Mechanika klasyczna');
