a4f2f86d30ad6cbaed1d47062bb512b11b658e6a
„Grzegorz <grzegorzgulba@gmail.com>
2024-06-17 12:00:57 +0200

    PiPAB lab. 7 - ZADANIE 2: Proszę w tym projekcie stworzyć klasy odpowiadające ilościowo i nazewniczo tabelom z bazy danych - Dodanie klasy Zajecia

---
+++ SOS/SOS/src/main/java/com/example/SOS/model/Zajecia.java
@@ -0,0 +1,30 @@
+package com.example.SOS.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Student")
+public class Zajecia {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id_zajec")
+    private int idZajec;
+    @Column(name = "id_przedmiotu")
+    private int idPrzedmiotu;
+    @Column(name = "id_wykladowcy")
+    private int idWykladowcy;
+    @Column(name = "id_sali")
+    private int idSali;
+    @Column(name = "forma_zajec")
+    private String formaZajec;
+    @Column(name = "status_zajec")
+    private String statusZajec;
+    @Column(name = "data_zajec")
+    private LocalDateTime dataZajec;
+
+}
