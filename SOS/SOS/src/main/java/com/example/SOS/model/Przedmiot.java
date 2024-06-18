d3d2dd0b677974ed7e727e18cbfe888d292e3ff9
„Grzegorz <grzegorzgulba@gmail.com>
2024-06-18 09:19:36 +0200

    PiPAB lab. 7 - ZADANIE 4: Proszę zadbać o settery i gettery - Dodanie getterów i setterów do klasy Przedmiot

--- SOS/SOS/src/main/java/com/example/SOS/model/Przedmiot.java
+++ SOS/SOS/src/main/java/com/example/SOS/model/Przedmiot.java
@@ -22,4 +22,36 @@ public class Przedmiot {
     @Column(name = "opis")
     private String opis;
 
+
+    public int getIdPrzedmiotu() {
+        return idPrzedmiotu;
+    }
+
+    public void setIdPrzedmiotu(int idPrzedmiotu) {
+        this.idPrzedmiotu = idPrzedmiotu;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public int getECTS() {
+        return ECTS;
+    }
+
+    public void setECTS(int ECTS) {
+        this.ECTS = ECTS;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
 }
