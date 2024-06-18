26b2a9a4d929dc667a5946d475a5241aa5bd4fa1
„Grzegorz <grzegorzgulba@gmail.com>
2024-06-18 09:12:16 +0200

    PiPAB lab. 7 - ZADANIE 4: Proszę zadbać o settery i gettery - Dodanie getterów i setterów do klasy Platnosci

--- SOS/SOS/src/main/java/com/example/SOS/model/Platnosci.java
+++ SOS/SOS/src/main/java/com/example/SOS/model/Platnosci.java
@@ -21,4 +21,36 @@ public class Platnosci {
     private double kwotaPlatnosci; // można dać BigDecimal
     @Column(name = "data_platnosci")
     private LocalDateTime dataPlatnosci;
+
+    public int getIdPlatnosci() {
+        return idPlatnosci;
+    }
+
+    public void setIdPlatnosci(int idPlatnosci) {
+        this.idPlatnosci = idPlatnosci;
+    }
+
+    public int getIdStudenta() {
+        return idStudenta;
+    }
+
+    public void setIdStudenta(int idStudenta) {
+        this.idStudenta = idStudenta;
+    }
+
+    public double getKwotaPlatnosci() {
+        return kwotaPlatnosci;
+    }
+
+    public void setKwotaPlatnosci(double kwotaPlatnosci) {
+        this.kwotaPlatnosci = kwotaPlatnosci;
+    }
+
+    public LocalDateTime getDataPlatnosci() {
+        return dataPlatnosci;
+    }
+
+    public void setDataPlatnosci(LocalDateTime dataPlatnosci) {
+        this.dataPlatnosci = dataPlatnosci;
+    }
 }
