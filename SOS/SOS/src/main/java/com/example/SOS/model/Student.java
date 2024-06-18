957026dcc95b94bea0479131654c16e8093ebe03
„Grzegorz <grzegorzgulba@gmail.com>
2024-06-18 09:34:18 +0200

    PiPAB lab. 7 - ZADANIE 4: Proszę zadbać o settery i gettery - Dodanie getterów i setterów do klasy Zajecia

--- SOS/SOS/src/main/java/com/example/SOS/model/Student.java
+++ SOS/SOS/src/main/java/com/example/SOS/model/Student.java
@@ -21,4 +21,37 @@ public class Student {
     private String nazwisko;
     @Column(name = "email")
     private String email;
+
+
+    public int getIdStudenta() {
+        return idStudenta;
+    }
+
+    public void setIdStudenta(int idStudenta) {
+        this.idStudenta = idStudenta;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
