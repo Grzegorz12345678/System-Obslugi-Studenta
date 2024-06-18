7b3fa55cab25b67046508e06cb8abdf7adc86adc
„Grzegorz <grzegorzgulba@gmail.com>
2024-06-18 09:17:52 +0200

    PiPAB lab. 7 - ZADANIE 4: Proszę zadbać o settery i gettery - Dodanie getterów i setterów do klasy Wykladowca

--- SOS/SOS/src/main/java/com/example/SOS/model/Wykladowca.java
+++ SOS/SOS/src/main/java/com/example/SOS/model/Wykladowca.java
@@ -24,4 +24,44 @@ public class Wykladowca {
     @Column(name = "tytul_naukowy")
     private String tytulNaukowy;
 
+
+    public int getIdWykladowcy() {
+        return idWykladowcy;
+    }
+
+    public void setIdWykladowcy(int idWykladowcy) {
+        this.idWykladowcy = idWykladowcy;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTytulNaukowy() {
+        return tytulNaukowy;
+    }
+
+    public void setTytulNaukowy(String tytulNaukowy) {
+        this.tytulNaukowy = tytulNaukowy;
+    }
 }
