b8bfa5f94545dd09b155cd5a44f2ab0dcca9acdf
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-06-26 09:34:26 +0200

    config

--- SOS/SOS/src/main/java/pl/wwsis/SOS/SosApplication.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/SosApplication.java
@@ -2,11 +2,14 @@ package pl.wwsis.SOS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring-config.xml")
 public class SosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SosApplication.class, args);
 	}
 
