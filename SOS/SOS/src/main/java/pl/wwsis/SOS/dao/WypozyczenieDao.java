0e25961213e2da07f48d5ab12eed32c488ccf916
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-02 16:29:55 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie WypozyczenieDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/WypozyczenieDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/WypozyczenieDao.java
@@ -1,4 +1,9 @@
 package pl.wwsis.SOS.dao;
 
+import java.util.Date;
+
 public interface WypozyczenieDao {
+    void wypozyczKsiazke(int idStudenta, int idKsiazki, Date termin);
+    void zwrocKsiazke(int idKsiazki);
+    boolean sprawdzDostepnosc(int idKsiazki);
 }
