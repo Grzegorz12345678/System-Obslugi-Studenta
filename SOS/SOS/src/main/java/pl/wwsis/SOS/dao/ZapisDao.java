4b4cdcbdefdb56bcbe2d3322e71616d564fcbfc1
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-02 16:22:59 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie ZapisDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/ZapisDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/ZapisDao.java
@@ -1,4 +1,11 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Przedmiot;
+import pl.wwsis.SOS.model.Student;
+
 public interface ZapisDao {
+
+    void zapiszStudenta(Student student, Przedmiot przedmiot);
+    void wypiszStudenta(Student student, Przedmiot przedmiot);
+
 }
