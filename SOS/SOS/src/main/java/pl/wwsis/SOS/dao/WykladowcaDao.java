65cd30675fecad4bb689c930560154f7abcd9b24
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-02 15:20:48 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie WykladowcaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/WykladowcaDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/WykladowcaDao.java
@@ -1,4 +1,9 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Ocena;
+
 public interface WykladowcaDao {
+    void dodajOcene(Ocena ocena);
+    void edytujOcene(Ocena ocena);
+    void usunOcene(Ocena ocena);
 }
