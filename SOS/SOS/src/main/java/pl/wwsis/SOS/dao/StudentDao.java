7682894a1608db3c33a0f8660c54e933708ad266
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 15:48:52 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie StudentDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/StudentDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/StudentDao.java
@@ -7,16 +7,7 @@ import pl.wwsis.SOS.model.Zajecia;
 import java.util.List;
 
 public interface StudentDao {
-
-    boolean zaloguj(String login, String hasło );
-    void zarejestruj(int numerIndeksu, String imie, String nazwisko, String login, String adressEmail, String haslo);
-    void wyloguj();
-    void zmienHaslo(String stareHaslo, String noweHaslo);
-    boolean edytujProfil(String imie, String nazwisko, String email, String haslo);
-    void przypomnijHaslo(String login);
-    void usunKonto();
-    void zablokujKonto();
-    List<Ocena> pobierzOceny();
-    List<Zajecia> pobierzZajecia();
-    List<Przedmiot> pobierzPrzedmioty();
+    List<Ocena> pobierzOceny(int idStudenta);
+    List<Zajecia> pobierzZajecia(int idStudenta);
+    List<Przedmiot> pobierzPrzedmioty(int idStudenta);
 }
