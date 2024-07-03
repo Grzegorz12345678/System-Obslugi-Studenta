98a1a5efb70dd44518db3d5a5d736b0dd222f056
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 18:11:40 +0200

    ZADANIE 3: Dokonaj implementacji wszystkich metod z interfejsów - implementacja interfejsu PrzedmiotDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/PrzedmiotDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/PrzedmiotDaoImpl.java
@@ -1,4 +1,33 @@
 package pl.wwsis.SOS.dao.impl;
 
-public class PrzedmiotDaoImpl {
+import pl.wwsis.SOS.dao.PrzedmiotDao;
+import pl.wwsis.SOS.model.Przedmiot;
+
+import java.util.List;
+
+public class PrzedmiotDaoImpl implements PrzedmiotDao {
+    @Override
+    public List<Przedmiot> pobierzWszystkiePrzedmioty() {
+        return null;
+    }
+
+    @Override
+    public Przedmiot pobierzPrzedmiot(int idPrzedmiotu) {
+        return null;
+    }
+
+    @Override
+    public void dodajPrzedmiot(Przedmiot przedmiot) {
+
+    }
+
+    @Override
+    public void edytujPrzedmiot(Przedmiot przedmiot) {
+
+    }
+
+    @Override
+    public void usunPrzedmiot(int idPrzedmiotu) {
+
+    }
 }
