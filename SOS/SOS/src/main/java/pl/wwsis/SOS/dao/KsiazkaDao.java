05760e74c218ee03f23e6144870bfb5bc4d61e47
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 16:27:53 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie KsiazkaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/KsiazkaDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/KsiazkaDao.java
@@ -1,4 +1,14 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Ksiazka;
+
+import java.util.List;
+
 public interface KsiazkaDao {
+    List<Ksiazka> pobierzWszystkieKsiazki();
+    Ksiazka pobierzKsiazke(int idKsiazki);
+    void dodajKsiazke(Ksiazka ksiazka);
+    void edytujKsiazke(Ksiazka ksiazka);
+    void usunKsiazke(int idKsiazki);
 }
+
