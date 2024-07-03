af587ea616064dcf2051af1477dddb0ed94e1081
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 16:02:46 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie OcenaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/OcenaDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/OcenaDao.java
@@ -1,4 +1,12 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Ocena;
+
+import java.util.List;
+
 public interface OcenaDao {
+    List<Ocena> pobierzOceny(int idStudenta);
+    void dodajOcene(int idStudenta, int idPrzedmiotu, Ocena ocena);
+    void edytujOcene(int idOceny, Ocena nowaOcena);
+    void usunOcene(int idOceny);
 }
