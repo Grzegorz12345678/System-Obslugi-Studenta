6cdbed2cc0b1af84dcb49c6cdc0666190ad0c956
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 18:18:25 +0200

    ZADANIE 3: Dokonaj implementacji wszystkich metod z interfejsów - implementacja interfejsu WykladowcaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/WykladowcaDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/WykladowcaDaoImpl.java
@@ -1,4 +1,21 @@
 package pl.wwsis.SOS.dao.impl;
 
-public class WykladowcaDaoImpl {
+import pl.wwsis.SOS.dao.WykladowcaDao;
+import pl.wwsis.SOS.model.Ocena;
+
+public class WykladowcaDaoImpl implements WykladowcaDao {
+    @Override
+    public void dodajOcene(Ocena ocena) {
+
+    }
+
+    @Override
+    public void edytujOcene(Ocena ocena) {
+
+    }
+
+    @Override
+    public void usunOcene(Ocena ocena) {
+
+    }
 }
