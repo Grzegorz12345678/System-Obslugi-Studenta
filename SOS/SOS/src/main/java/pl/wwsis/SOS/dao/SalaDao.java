ab11fab0f9ca2be1b1a25a8347b1c96aba827720
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 16:14:21 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie SalaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/SalaDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/SalaDao.java
@@ -1,4 +1,11 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Sala;
+
+import java.util.List;
+
 public interface SalaDao {
+    List<Sala> pobierzWszystkieSale();
+    Sala pobierzSale(int idSali);
 }
+
