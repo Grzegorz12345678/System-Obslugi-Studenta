232b2028ae9e2c48a94fda94103faf9871972025
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 15:43:44 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie KontoDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/KontoDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/KontoDao.java
@@ -1,4 +1,13 @@
 package pl.wwsis.SOS.dao;
 
 public interface KontoDao {
+    void zarejestruj(int numerIndeksu, String imie, String nazwisko, String login, String adresEmail, String haslo);
+    boolean zaloguj(String login, String haslo);
+    void wyloguj();
+    void zmienHaslo(String stareHaslo, String noweHaslo);
+    boolean edytujProfil(String imie, String nazwisko, String email, String haslo);
+    void przypomnijHaslo(String login);
+    void usunKonto();
+    void zablokujKonto();
+    boolean jestZablokowane(String login);
 }
