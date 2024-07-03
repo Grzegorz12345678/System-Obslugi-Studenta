966566249d276d8e3e9cc49bd24b59f9d57558a8
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 16:05:45 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie PlatnosciDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/PlatnosciDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/PlatnosciDao.java
@@ -1,4 +1,11 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Platnosci;
+
+import java.util.List;
+
 public interface PlatnosciDao {
+    List<Platnosci> pobierzPlatnosci(int idStudenta);
+    boolean czyZaplacone(int idStudenta);
 }
+
