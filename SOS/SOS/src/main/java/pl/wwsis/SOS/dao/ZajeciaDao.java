b9a5d88ecebd89ccb231d4106280e7becb0dd74d
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 17:18:41 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie ZajeciaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/ZajeciaDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/ZajeciaDao.java
@@ -1,4 +1,14 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Zajecia;
+
+import java.util.List;
+
 public interface ZajeciaDao {
+    List<Zajecia> pobierzWszystkieZajecia();
+    Zajecia pobierzZajecia(int idZajecia);
+    void dodajZajecia(Zajecia zajecia);
+    void edytujZajecia(Zajecia zajecia);
+    void usunZajecia(int idZajecia);
 }
+
