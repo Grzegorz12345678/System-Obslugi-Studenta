6f36606046ca2e78cad7277d22d62e51e315d432
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 16:19:03 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie SesjaUzytkownikaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/SesjaUzytkownikaDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/SesjaUzytkownikaDao.java
@@ -1,4 +1,8 @@
 package pl.wwsis.SOS.dao;
 
 public interface SesjaUzytkownikaDao {
+    void utworzSesje(int idUzytkownika);
+    void zakonczSesje(int idUzytkownika);
+    boolean czySesjaAktywna(int idUzytkownika);
 }
+
