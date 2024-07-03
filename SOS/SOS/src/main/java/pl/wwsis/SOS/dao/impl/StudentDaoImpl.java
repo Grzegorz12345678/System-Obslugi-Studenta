18944b14cf5abdb668098609d07893756fc2af31
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 18:14:30 +0200

    ZADANIE 3: Dokonaj implementacji wszystkich metod z interfejsów - implementacja interfejsu StudentDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/StudentDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/StudentDaoImpl.java
@@ -1,4 +1,25 @@
 package pl.wwsis.SOS.dao.impl;
 
-public class StudentDaoImpl{
+import pl.wwsis.SOS.dao.StudentDao;
+import pl.wwsis.SOS.model.Ocena;
+import pl.wwsis.SOS.model.Przedmiot;
+import pl.wwsis.SOS.model.Zajecia;
+
+import java.util.List;
+
+public class StudentDaoImpl implements StudentDao {
+    @Override
+    public List<Ocena> pobierzOceny(int idStudenta) {
+        return null;
+    }
+
+    @Override
+    public List<Zajecia> pobierzZajecia(int idStudenta) {
+        return null;
+    }
+
+    @Override
+    public List<Przedmiot> pobierzPrzedmioty(int idStudenta) {
+        return null;
+    }
 }
