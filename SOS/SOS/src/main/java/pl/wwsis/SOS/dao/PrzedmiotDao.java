5e0ec8e7ca997156daa57027811d0b67d164193d
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-03 16:10:48 +0200

    ZADANIE 1: W interfejsach *Dao deklarujemy metody które będą implementować logikę opisaną w fazie projektowania (głównie: diagram klas oraz wymagania funkcjonalne) - deklaracja metod w interfejsie PrzedmiotDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/PrzedmiotDao.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/PrzedmiotDao.java
@@ -1,4 +1,12 @@
 package pl.wwsis.SOS.dao;
 
+import pl.wwsis.SOS.model.Przedmiot;
+
+import java.util.List;
+
 public interface PrzedmiotDao {
+    List<Przedmiot> pobierzWszystkiePrzedmioty();
+    Przedmiot pobierzPrzedmiot(int idPrzedmiotu);
+
 }
+
