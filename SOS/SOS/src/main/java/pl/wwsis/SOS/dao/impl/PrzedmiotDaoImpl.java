fab9b5a60653652c4476da95589c4d5d1fd0878e
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-07-05 11:21:10 +0200

    wykonywanie listy

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/PrzedmiotDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/PrzedmiotDaoImpl.java
@@ -2,32 +2,47 @@ package pl.wwsis.SOS.dao.impl;
 
 import pl.wwsis.SOS.dao.PrzedmiotDao;
 import pl.wwsis.SOS.model.Przedmiot;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
+@Repository
+@Transactional
 public class PrzedmiotDaoImpl implements PrzedmiotDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Przedmiot> pobierzWszystkiePrzedmioty() {
-        return null;
+        Query query = entityManager.createQuery("SELECT p FROM Przedmiot p");
+        return query.getResultList();
     }
 
     @Override
     public Przedmiot pobierzPrzedmiot(int idPrzedmiotu) {
-        return null;
+        return entityManager.find(Przedmiot.class, idPrzedmiotu);
     }
 
     @Override
     public void dodajPrzedmiot(Przedmiot przedmiot) {
-
+        entityManager.persist(przedmiot);
     }
 
     @Override
     public void edytujPrzedmiot(Przedmiot przedmiot) {
-
+        entityManager.merge(przedmiot);
     }
 
     @Override
     public void usunPrzedmiot(int idPrzedmiotu) {
-
+        Przedmiot przedmiot = entityManager.find(Przedmiot.class, idPrzedmiotu);
+        if (przedmiot != null) {
+            entityManager.remove(przedmiot);
+        }
     }
 }
