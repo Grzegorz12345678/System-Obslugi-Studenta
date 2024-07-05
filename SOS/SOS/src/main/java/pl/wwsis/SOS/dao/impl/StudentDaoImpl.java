9fd4254ac42b507aa4e8e56adffde79a8c6594e5
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-07-05 11:22:47 +0200

    wykonywanie listy

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/StudentDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/StudentDaoImpl.java
@@ -4,22 +4,39 @@ import pl.wwsis.SOS.dao.StudentDao;
 import pl.wwsis.SOS.model.Ocena;
 import pl.wwsis.SOS.model.Przedmiot;
 import pl.wwsis.SOS.model.Zajecia;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.List;
 
+@Repository
+@Transactional
 public class StudentDaoImpl implements StudentDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Ocena> pobierzOceny(int idStudenta) {
-        return null;
+        Query query = entityManager.createQuery("SELECT o FROM Ocena o WHERE o.student.idStudenta = :idStudenta");
+        query.setParameter("idStudenta", idStudenta);
+        return query.getResultList();
     }
 
     @Override
     public List<Zajecia> pobierzZajecia(int idStudenta) {
-        return null;
+        Query query = entityManager.createQuery("SELECT z FROM Zajecia z JOIN z.students s WHERE s.idStudenta = :idStudenta");
+        query.setParameter("idStudenta", idStudenta);
+        return query.getResultList();
     }
 
     @Override
     public List<Przedmiot> pobierzPrzedmioty(int idStudenta) {
-        return null;
+        Query query = entityManager.createQuery("SELECT p FROM Przedmiot p JOIN p.zajecia z JOIN z.students s WHERE s.idStudenta = :idStudenta");
+        query.setParameter("idStudenta", idStudenta);
+        return query.getResultList();
     }
 }
