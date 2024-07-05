71344d795f6a55bbbc6e8f55665fd8c353acaf5d
Grzegorz_7584 <grzesiek7931@o2.pl>
2024-07-05 11:24:14 +0200

    wykonywanie listy

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/WykladowcaDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/WykladowcaDaoImpl.java
@@ -1,21 +1,33 @@
 package pl.wwsis.SOS.dao.impl;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.wwsis.SOS.dao.WykladowcaDao;
 import pl.wwsis.SOS.model.Ocena;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Transactional
 public class WykladowcaDaoImpl implements WykladowcaDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public void dodajOcene(Ocena ocena) {
-
+        entityManager.persist(ocena);
     }
 
     @Override
     public void edytujOcene(Ocena ocena) {
-
+        entityManager.merge(ocena);
     }
 
     @Override
     public void usunOcene(Ocena ocena) {
-
+        Ocena mergedOcena = entityManager.merge(ocena);
+        entityManager.remove(mergedOcena);
     }
 }
