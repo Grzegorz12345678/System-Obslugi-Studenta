f7b6326f0f37b2550c1828f37725f477d226c41b
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-08 15:09:43 +0200

    ZADANIE 3: Dokonaj implementacji wszystkich metod z interfejsów - implementacja interfejsu OcenaDao

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/OcenaDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/OcenaDaoImpl.java
@@ -1,4 +1,28 @@
 package pl.wwsis.SOS.dao.impl;
 
-public class OcenaDaoImpl {
+import pl.wwsis.SOS.dao.OcenaDao;
+import pl.wwsis.SOS.model.Ocena;
+
+import java.util.List;
+
+public class OcenaDaoImpl implements OcenaDao {
+    @Override
+    public List<Ocena> pobierzOceny(int idStudenta) {
+        return null;
+    }
+
+    @Override
+    public void dodajOcene(int idStudenta, int idPrzedmiotu, Ocena ocena) {
+
+    }
+
+    @Override
+    public void edytujOcene(int idOceny, Ocena nowaOcena) {
+
+    }
+
+    @Override
+    public void usunOcene(int idOceny) {
+
+    }
 }
