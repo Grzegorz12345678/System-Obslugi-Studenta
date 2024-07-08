f58a063147d25cb61ca10f89e0f33b42d565fe2b
„Grzegorz <grzegorzgulba@gmail.com>
2024-07-08 15:17:52 +0200

    ZADANIE 3: Dokonaj implementacji wszystkich metod z interfejsów - implementacja interfejsu OcenaDao - metoda pobierzOceny

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/OcenaDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/OcenaDaoImpl.java
@@ -3,12 +3,20 @@ package pl.wwsis.SOS.dao.impl;
 import pl.wwsis.SOS.dao.OcenaDao;
 import pl.wwsis.SOS.model.Ocena;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class OcenaDaoImpl implements OcenaDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Ocena> pobierzOceny(int idStudenta) {
-        return null;
+        TypedQuery<Ocena> query = entityManager.createQuery("SELECT o FROM Ocena o WHERE o.idStudenta = :idStudenta", Ocena.class);
+        query.setParameter("idStudenta", idStudenta);
+        return query.getResultList();
     }
 
     @Override
