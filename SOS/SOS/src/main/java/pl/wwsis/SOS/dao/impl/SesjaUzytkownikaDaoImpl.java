9a65cf287d1fd262fb2d15bd68d304547bc115f9
Przemysław Stańczyk <stanczykps@gmail.com>
2024-07-09 19:43:18 +0200

    implementacja klasy SesjaUzytkownikaDaoImpl

--- SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/SesjaUzytkownikaDaoImpl.java
+++ SOS/SOS/src/main/java/pl/wwsis/SOS/dao/impl/SesjaUzytkownikaDaoImpl.java
@@ -1,4 +1,54 @@
 package pl.wwsis.SOS.dao.impl;
 
-public class SesjaUzytkownikaDaoImpl {
+import pl.wwsis.SOS.dao.SesjaUzytkownikaDao;
+import pl.wwsis.SOS.model.SesjaUzytkownika;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.time.LocalDateTime;
+
+@Repository
+@Transactional
+public class SesjaUzytkownikaDaoImpl implements SesjaUzytkownikaDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void utworzSesje(int idUzytkownika) {
+        SesjaUzytkownika sesja = new SesjaUzytkownika();
+        sesja.setIdKonta(idUzytkownika);
+        sesja.setStartSesji(LocalDateTime.now());
+        sesja.setIP("127.0.0.1");
+        entityManager.persist(sesja);
+    }
+
+    @Override
+    public void zakonczSesje(int idUzytkownika) {
+        TypedQuery<SesjaUzytkownika> query = entityManager.createQuery(
+                "SELECT s FROM SesjaUzytkownika s WHERE s.idKonta = :idKonta AND s.koniecSesji IS NULL",
+                SesjaUzytkownika.class);
+        query.setParameter("idKonta", idUzytkownika);
+        List<SesjaUzytkownika> sesje = query.getResultList();
+
+        if (!sesje.isEmpty()) {
+            SesjaUzytkownika sesja = sesje.get(0);
+            sesja.setKoniecSesji(LocalDateTime.now());
+            entityManager.merge(sesja);
+        }
+    }
+
+    @Override
+    public boolean czySesjaAktywna(int idUzytkownika) {
+        TypedQuery<SesjaUzytkownika> query = entityManager.createQuery(
+                "SELECT s FROM SesjaUzytkownika s WHERE s.idKonta = :idKonta AND s.koniecSesji IS NULL",
+                SesjaUzytkownika.class);
+        query.setParameter("idKonta", idUzytkownika);
+        List<SesjaUzytkownika> sesje = query.getResultList();
+
+        return !sesje.isEmpty();
+    }
 }
